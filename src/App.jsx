3086b501ec632e2d607819da3f00e17a4cd219e5
Bozhidar Videnov <bozh.bv03@gmail.com>
2024-01-31 22:09:54 +0200

    Added layout and basic functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import { TextField, TableContainer, Table, TableHead, TableRow, TableCell, TableBody, Paper} from '@mui/material'
+import Fuse from 'fuse.js'
+import questions from './questions.json'
+import { v4 as uuid4 } from 'uuid'
+
+const FUSE_OPTIONS = {
+	// isCaseSensitive: false,
+	// includeScore: false,
+	// shouldSort: true,
+	// includeMatches: false,
+	// findAllMatches: false,
+	// minMatchCharLength: 1,
+	// location: 0,
+	// threshold: 0.6,
+	// distance: 100,
+	// useExtendedSearch: false,
+	// ignoreLocation: false,
+	// ignoreFieldNorm: false,
+	// fieldNormWeight: 1,
+	keys: [
+		"q"
+	]
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [input, setInput] = useState("")
+  const [options, setOptions] = useState([])
+  const fuse = new Fuse(questions, FUSE_OPTIONS)
+
+  useEffect(() => {
+    setOptions(fuse.search(input))
+  }, [input])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-        <h1>Is it working</h1>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <TextField value={input} onChange={e => setInput(e.target.value)} color='info' sx={{input: { color: 'white' }}}/>
+      <TableContainer component={Paper} sx={{marginTop: 10}}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Question</TableCell>
+            <TableCell align="right">Answer</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {options.map((option) => (
+            <TableRow
+              key={option.refIndex}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {option.item.q}
+              </TableCell>
+              <TableCell  align="right">{option.item.a}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
     </>
   )
 }
